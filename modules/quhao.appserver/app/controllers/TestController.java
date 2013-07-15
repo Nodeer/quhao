a7b3777ed43aa84e5cd660b224c62c184cddb597
Cross_Lee <mag_lee@126.com>
2013-07-15 17:34:15 +0800

    add json.jar and demo code for json

--- modules/quhao.appserver/app/controllers/TestController.java
+++ modules/quhao.appserver/app/controllers/TestController.java
@@ -7,6 +7,9 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.withiter.models.test.Test;
 
 public class TestController extends BaseController {
@@ -22,12 +25,13 @@ public class TestController extends BaseController {
 		String strUrl = "http://localhost:9081/testcontroller/test1?arg=2222";
 
         URL url = null;
+        String result = "";
         try {
 			url = new URL(strUrl);
 			HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
 			InputStreamReader in = new InputStreamReader(urlConn.getInputStream());
 			BufferedReader br = new BufferedReader(in);
-			String result = "";
+			
 			String readerLine = null;
 			while((readerLine=br.readLine())!=null){
 				result += readerLine;
@@ -35,14 +39,27 @@ public class TestController extends BaseController {
 			in.close();
 			urlConn.disconnect();
 			System.out.println("result:"+result);
-//			TextView textView = (TextView)this.findViewById(R.id.result);
-//			textView.setText(result);
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        
+        try {
+			String str = "{\"a\":\"b\", \"c\":\"d\"}";
+			JSONObject a = new JSONObject(str);
+			System.out.println(a); // {"c":"d","a":"b"}
+			System.out.println(a.get("c")); // d
+			
+			String jsonStr = result;
+			JSONObject object = new JSONObject(jsonStr);
+			System.out.println(object.get("name"));
+			System.out.println(object.get("age"));
+			
+		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
 }
