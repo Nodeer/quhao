9e0e314980356386685b9eeab4a185e577ecc9d3
Cross_Lee <mag_lee@126.com>
2013-07-23 14:52:22 +0800

    add backend controller package

---
+++ modules/quhao.appserver/app/controllers/backend/AccountController.java
@@ -0,0 +1,10 @@
+package controllers.backend;
+
+import controllers.BaseController;
+
+public class AccountController extends BaseController {
+
+	public static void test(){
+		renderJSON("aaa");
+	}
+}
