91e44a75db43ffaaca55765fe3fbcd416357f984
Cross Lee <mag_lee@126.com>
2013-07-24 23:31:47 +0800

    add haoma entity

--- modules/quhao.appserver/app/com/withiter/models/merchant/HaomaEntityDef.java
+++ modules/quhao.appserver/app/com/withiter/models/merchant/HaomaEntityDef.java
@@ -4,11 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.google.code.morphia.annotations.Entity;
+import com.google.code.morphia.annotations.Indexed;
 import com.withiter.models.BaseModel;
 
 @Entity
 public abstract class HaomaEntityDef extends BaseModel {
 
+	@Indexed
+	String merchantId;
 	Map<Integer, Integer> haoma = new HashMap<Integer, Integer>();
 }
 
