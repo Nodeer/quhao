b85334d79cbc403bf9db46375e44b7e9384c6bd6
Cross_Lee <mag_lee@126.com>
2013-08-02 17:57:37 +0800

    add status to reservationVO

--- modules/quhao.appserver/app/vo/ReservationVO.java
+++ modules/quhao.appserver/app/vo/ReservationVO.java
@@ -1,5 +1,6 @@
 package vo;
 
+import com.withiter.common.Constants;
 import com.withiter.models.account.Reservation;
 
 public class ReservationVO {
@@ -12,6 +13,8 @@ public class ReservationVO {
 	public int seatNumber;
 	public int myNumber;
 	public boolean valid;
+	public Constants.ReservationStatus status;
+	
 	
 	public void build(Reservation r){
 		this.accountId = r.accountId;
@@ -19,5 +22,6 @@ public class ReservationVO {
 		this.myNumber = r.myNumber;
 		this.seatNumber = r.seatNumber;
 		this.valid = r.valid;
+		this.status = r.status;
 	}
 }
