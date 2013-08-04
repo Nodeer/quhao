cca26e195266713d48b6b0d38602d8ab380cc8d9
Cross Lee <mag_lee@126.com>
2013-08-04 23:10:48 +0800

    update

--- modules/quhao.appserver/app/com/withiter/common/lbs/business/DirectionBusiness.java
+++ modules/quhao.appserver/app/com/withiter/common/lbs/business/DirectionBusiness.java
@@ -18,19 +18,18 @@ import com.withiter.common.lbs.bean.Distance;
 public class DirectionBusiness {
 
 	private static final String key = Play.configuration
-			.getProperty("develop.direction.app.key");
+			.getProperty("develop.direction.app.ak");
 	private static final String serviceUrl = Play.configuration
 			.getProperty("service.direction.baseurl");
 
 	// http://api.map.baidu.com/direction/v1?mode=driving&origin=%E6%B8%85%E5%8D%8E%E5%A4%A7%E5%AD%A6&destination=%E5%8C%97%E4%BA%AC%E5%A4%A7%E5%AD%A6&origin_region=%E5%8C%97%E4%BA%AC&destination_region=%E5%8C%97%E4%BA%AC&output=json&ak=E4805d16520de693a3fe707cdc962045
 
 	public static Distance direction(String origin, String destination,
-			String mode, String region, String origin_region,
-			String destination_region, String output, String ak) {
+			String mode, String region, String origin_region, String destination_region, String output, String ak) {
 		Distance d = new Distance();
 		if (StringUtils.isEmpty(origin) || StringUtils.isEmpty(destination)
-				|| StringUtils.isEmpty(origin_region)
-				|| StringUtils.isEmpty(destination_region)
+//				|| StringUtils.isEmpty(origin_region)
+//				|| StringUtils.isEmpty(destination_region)
 				|| StringUtils.isEmpty(key)) {
 			return null;
 		}
@@ -51,19 +50,19 @@ public class DirectionBusiness {
 		}
 
 		if (mode.equalsIgnoreCase(Constants.DirectionMode.driving.toString())) {
-			sb.append("&origin=").append(origin).append("&destination=")
-					.append(destination).append("&origin_region=")
-					.append(origin_region).append("&destination_region=")
-					.append(destination_region).append("&output=")
-					.append(output).append("&ak=").append(key);
+			sb.append("&origin=").append(origin)
+					.append("&destination=").append(destination)
+					.append("&origin_region=").append(origin_region)
+					.append("&destination_region=").append(destination_region)
+					.append("&output=").append(output).append("&ak=").append(key);
 			requestUrl = sb.toString();
 		}
 
 		if (!mode.equalsIgnoreCase(Constants.DirectionMode.driving.toString())) {
 			sb.append("&origin=").append(origin).append("&destination=")
 					.append("&region=").append(region).append(destination)
-					.append("&origin_region=").append(origin_region)
-					.append("&destination_region=").append(destination_region)
+//					.append("&origin_region=").append(origin_region)
+//					.append("&destination_region=").append(destination_region)
 					.append("&output=").append(output).append("&ak=")
 					.append(key);
 			requestUrl = sb.toString();
