f047e512029a374ee462fd64a8dfecb701fe7504
Cross_Lee <mag_lee@126.com>
2013-08-06 10:25:01 +0800

    distance update

--- modules/quhao.appserver/app/com/withiter/common/lbs/business/DirectionBusiness.java
+++ modules/quhao.appserver/app/com/withiter/common/lbs/business/DirectionBusiness.java
@@ -25,15 +25,16 @@ public class DirectionBusiness {
 	private static final String serviceUrl = Play.configuration
 			.getProperty("service.direction.baseurl");
 
-	// http://api.map.baidu.com/direction/v1?mode=driving&origin=%E6%B8%85%E5%8D%8E%E5%A4%A7%E5%AD%A6&destination=%E5%8C%97%E4%BA%AC%E5%A4%A7%E5%AD%A6&origin_region=%E5%8C%97%E4%BA%AC&destination_region=%E5%8C%97%E4%BA%AC&output=json&ak=E4805d16520de693a3fe707cdc962045
+	// 	for driving
+	//	http://api.map.baidu.com/direction/v1?mode=driving&origin=%E6%B8%85%E5%8D%8E%E5%A4%A7%E5%AD%A6&destination=%E5%8C%97%E4%BA%AC%E5%A4%A7%E5%AD%A6&origin_region=%E5%8C%97%E4%BA%AC&destination_region=%E5%8C%97%E4%BA%AC&output=json&ak=E4805d16520de693a3fe707cdc962045
+	
+	//	for transit
 	//	http://api.map.baidu.com/direction/v1?mode=transit&origin=%E4%B8%8A%E5%9C%B0%E4%BA%94%E8%A1%97&destination=%E5%8C%97%E4%BA%AC%E5%A4%A7%E5%AD%A6&region=%E5%8C%97%E4%BA%AC&output=json&ak=E4805d16520de693a3fe707cdc962045
 	
 	public static Distance direction(String origin, String destination,
 			String mode, String region, String origin_region, String destination_region, String output, String ak) throws JSONException {
 		Distance d = new Distance();
 		if (StringUtils.isEmpty(origin) || StringUtils.isEmpty(destination)
-//				|| StringUtils.isEmpty(origin_region)
-//				|| StringUtils.isEmpty(destination_region)
 				|| StringUtils.isEmpty(key)) {
 			return null;
 		}
@@ -43,11 +44,7 @@ public class DirectionBusiness {
 		if (StringUtils.isEmpty(mode)) {
 			mode = Constants.DirectionMode.driving.toString();
 		}
-
 		sb.append("?mode=").append(mode);
-//		if (StringUtils.isEmpty(mode)) {
-//			mode = Constants.DirectionMode.driving.toString();
-//		}
 
 		if (StringUtils.isEmpty(output)) {
 			output = "json";
@@ -65,8 +62,6 @@ public class DirectionBusiness {
 		if (!mode.equalsIgnoreCase(Constants.DirectionMode.driving.toString())) {
 			sb.append("&origin=").append(origin).append("&destination=")
 					.append("&region=").append(region).append(destination)
-//					.append("&origin_region=").append(origin_region)
-//					.append("&destination_region=").append(destination_region)
 					.append("&output=").append(output).append("&ak=")
 					.append(key);
 			requestUrl = sb.toString();
