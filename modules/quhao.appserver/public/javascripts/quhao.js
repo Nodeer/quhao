e9f2052e9ddfd0657abe5a7ea7f45c29cf7d16bb
Cross_Lee <mag_lee@126.com>
2013-08-15 14:42:27 +0800

    add comments into quhao.js

--- modules/quhao.appserver/public/javascripts/quhao.js
+++ modules/quhao.appserver/public/javascripts/quhao.js
@@ -1,21 +1,39 @@
+/**
+ * Quhao is also a common function.
+ */
+
 Quhao = {};
 
+/**
+ * print into browser console.
+ */
 Quhao.log = function(msg) {
 	window.console && console.log(msg);
 }
 
+/**
+ * replace blank string at the beginning and end of parameter str
+ */
 Quhao.trim = function(str) {
 	if(str == null || str == ""){
 		return "";
 	}
 	return str.replace(/(^\s*)|(\s*$)/g, "");
 };
+
+/**
+ * replace blank string at the beginning of parameter str
+ */
 Quhao.ltrim = function(str) {
 	if(str == null || str == ""){
 		return "";
 	}
 	return str.replace(/(^\s*)/g, "");
 };
+
+/**
+ * replace blank string at the end of parameter str
+ */
 Quhao.rtrim = function(str) {
 	if(str == null || str == ""){
 		return "";
