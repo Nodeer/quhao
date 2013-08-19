d110e647e49eb41678255776ce42f2dd493d3e40
Cross Lee <mag_lee@126.com>
2013-08-19 22:12:17 +0800

    personal ip test

---
+++ modules/quhao.appserver/app/com/withiter/test/PersonalIPTest.java
@@ -0,0 +1,19 @@
+package com.withiter.test;
+
+public class PersonalIPTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String ip = "";
+		String userHome = System.getProperty("user.home");
+		System.out.println(userHome);
+		if(userHome.contains("cross")){
+			ip = "192.168.1.20";
+		}
+		if(userHome.contains("jazze")){
+			ip="xxx.xxx.xxx.xxx";
+		}
+	}
+
+}
