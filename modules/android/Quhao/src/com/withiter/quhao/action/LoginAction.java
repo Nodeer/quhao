e74c776d8d2b9dd7035186d421cbad4b2edae727
Cross_Lee <mag_lee@126.com>
2013-08-20 17:54:19 +0800

    formate code

--- modules/android/Quhao/src/com/withiter/quhao/action/LoginAction.java
+++ modules/android/Quhao/src/com/withiter/quhao/action/LoginAction.java
@@ -1,26 +1,20 @@
 package com.withiter.quhao.action;
 
-public class LoginAction
-{
+public class LoginAction {
 
 	private final static String TABLENAME = "accountinfo";
-	
 	private final static String TAG = "LoginAction";
-	
 	private static LoginAction instance = new LoginAction();
-	
-	private LoginAction()
-	{
-		
+
+	private LoginAction() {
+
 	}
-	
-	public static LoginAction getInstance()
-	{
+
+	public static LoginAction getInstance() {
 		return instance;
 	}
 
-	public Object queryUserId()
-	{
+	public Object queryUserId() {
 		// TODO Auto-generated method stub
 		return null;
 	}
