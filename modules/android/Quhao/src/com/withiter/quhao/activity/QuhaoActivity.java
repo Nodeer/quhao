8444579ae8fe1fc8c6a1aba1e11f9930d8da3bea
Cross_Lee <mag_lee@126.com>
2013-08-20 17:54:45 +0800

    formate code

--- modules/android/Quhao/src/com/withiter/quhao/activity/QuhaoActivity.java
+++ modules/android/Quhao/src/com/withiter/quhao/activity/QuhaoActivity.java
@@ -2,7 +2,6 @@ package com.withiter.quhao.activity;
 
 import android.app.Activity;
 
-public abstract class QuhaoActivity extends Activity
-{
-	
+public abstract class QuhaoActivity extends Activity {
+
 }
