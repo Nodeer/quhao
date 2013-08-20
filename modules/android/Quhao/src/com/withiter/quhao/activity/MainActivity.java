b4ecd579c748b632504d5b6fcfaad9b423ca9d09
Cross_Lee <mag_lee@126.com>
2013-08-20 17:45:16 +0800

    update

--- modules/android/Quhao/src/com/withiter/quhao/activity/MainActivity.java
+++ modules/android/Quhao/src/com/withiter/quhao/activity/MainActivity.java
@@ -35,38 +35,25 @@ import com.withiter.quhao.view.InnerListView;
 import com.withiter.quhao.vo.Category;
 import com.withiter.quhao.vo.TopMerchant;
 
-public class MainActivity extends AppStoreActivity
-{
+public class MainActivity extends AppStoreActivity {
 
 	private String LOGTAG = MainActivity.class.getName();
-	
 	protected ListView topMerchantListView;
-	
 	protected InnerListView categorysListView;
-	
 	private GridView topMerchantsGird;
-	
 	private List<TopMerchant> topMerchants;
-	
 	private GridView categorysGird;
-	
 	private DisplayMetrics localDisplayMetrics;
-	
 	protected ProgressDialogUtil progressCategory;
-	
 	protected ProgressDialogUtil progressTopMerchant;
-	
 	private final int UNLOCK_CLICK = 1000;
-	
 	private boolean isClick = false;
-	
 	private List<Category> categorys = null;
-	
+
 	/**
 	 * handler处理 解锁的时候可能会关闭其他的等待提示框
 	 */
-	private Handler unlockHandler = new Handler() 
-	{
+	private Handler unlockHandler = new Handler() {
 		public void handleMessage(Message msg) {
 			if (msg.what == UNLOCK_CLICK) {
 				// 解锁
@@ -74,63 +61,53 @@ public class MainActivity extends AppStoreActivity
 			}
 		}
 	};
-	
-	private Handler topMerchantsUpdateHandler = new Handler()
-	{
 
+	private Handler topMerchantsUpdateHandler = new Handler() {
 		@Override
-		public void handleMessage(Message msg)
-		{
-			if(msg.what == 200)
-			{
+		public void handleMessage(Message msg) {
+			if (msg.what == 200) {
 				super.handleMessage(msg);
-				ListAdapter adapter = new TopMerchantGridAdapter(topMerchants, topMerchantsGird,MainActivity.this);
+				ListAdapter adapter = new TopMerchantGridAdapter(topMerchants,
+						topMerchantsGird, MainActivity.this);
 				topMerchantsGird.setAdapter(adapter);
 				unlockHandler.sendEmptyMessageDelayed(UNLOCK_CLICK, 1000);
 			}
-			
+
 		}
 
 	};
-	
-	private Handler categorysUpdateHandler = new Handler()
-	{
 
+	private Handler categorysUpdateHandler = new Handler() {
 		@Override
-		public void handleMessage(Message msg)
-		{
-			if(msg.what == 200)
-			{
+		public void handleMessage(Message msg) {
+			if (msg.what == 200) {
 				super.handleMessage(msg);
-				
-				ListAdapter adapter = new CategoryGridAdapter(categorys, categorysGird,MainActivity.this);
+
+				ListAdapter adapter = new CategoryGridAdapter(categorys,
+						categorysGird, MainActivity.this);
 				categorysGird.setAdapter(adapter);
-				
+
 				unlockHandler.sendEmptyMessageDelayed(UNLOCK_CLICK, 1000);
 			}
-			
+
 		}
 
 	};
-	
-	
-	
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.main_layout);
 		super.onCreate(savedInstanceState);
-		
-		//initView();
+
+		// initView();
 		localDisplayMetrics = getResources().getDisplayMetrics();
-		
+
 		topMerchants = new ArrayList<TopMerchant>();
-		
+
 		topMerchantsGird = (GridView) findViewById(R.id.topMerchants);
 		getTopMerchants();
-		
-		
+
 		topMerchantsGird.setOnItemClickListener(topMerchantClickListener);
 		categorys = new ArrayList<Category>();
 		categorysGird = (GridView) findViewById(R.id.categorys);
@@ -141,13 +118,10 @@ public class MainActivity extends AppStoreActivity
 		btnMarchent.setOnClickListener(getMarchentListListener(this));
 	}
 
-	private OnItemClickListener topMerchantClickListener = new OnItemClickListener()
-	{
-
+	private OnItemClickListener topMerchantClickListener = new OnItemClickListener() {
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position,
-				long id)
-		{
+				long id) {
 			String topMerchantId = topMerchants.get(position).id;
 			Intent intent = new Intent();
 			intent.putExtra("id", topMerchantId);
@@ -156,159 +130,139 @@ public class MainActivity extends AppStoreActivity
 			overridePendingTransition(R.anim.main_enter, R.anim.main_exit);
 		}
 	};
-	
-	private OnItemClickListener categorysClickListener = new OnItemClickListener()
-	{
 
+	private OnItemClickListener categorysClickListener = new OnItemClickListener() {
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position,
-				long id)
-		{
+				long id) {
 			Category category = categorys.get(position);
-			Log.d(LOGTAG, "the category is : " + category.categoryType + "the count is : " + category.count);
+			Log.d(LOGTAG, "the category is : " + category.categoryType
+					+ "the count is : " + category.count);
 			Intent intent = new Intent();
-			intent.putExtra("categoryType", category.categoryType); 
-			intent.putExtra("categoryTypeStr", category.categoryTypeStr); 
-			intent.putExtra("categoryCount", String.valueOf(category.count)); 
+			intent.putExtra("categoryType", category.categoryType);
+			intent.putExtra("categoryTypeStr", category.categoryTypeStr);
+			intent.putExtra("categoryCount", String.valueOf(category.count));
 			intent.setClass(MainActivity.this, MerchantsActivity.class);
 			startActivity(intent);
 			overridePendingTransition(R.anim.main_enter, R.anim.main_exit);
 		}
 	};
-	
-	private void getTopMerchants()
-	{
-		progressTopMerchant = new ProgressDialogUtil(this, R.string.empty, R.string.querying, false);
+
+	private void getTopMerchants() {
+		progressTopMerchant = new ProgressDialogUtil(this, R.string.empty,
+				R.string.querying, false);
 		progressTopMerchant.showProgress();
-		
+
 		Thread topMerchantsThread = new Thread(topMerchantsRunnable);
 		topMerchantsThread.start();
 	}
-	
-	private void getCateGorys()
-	{
-		if(isClick)
-		{
+
+	private void getCateGorys() {
+		if (isClick) {
 			return;
 		}
-		
+
 		isClick = true;
-		
-		progressCategory = new ProgressDialogUtil(this, R.string.empty, R.string.querying, false);
+		progressCategory = new ProgressDialogUtil(this, R.string.empty,
+				R.string.querying, false);
 		progressCategory.showProgress();
-		
+
 		Thread categoryThread = new Thread(categoryRunnable);
 		categoryThread.start();
 	}
 
-	private Runnable topMerchantsRunnable = new Runnable()
-	{
-		
+	private Runnable topMerchantsRunnable = new Runnable() {
 		@Override
-		public void run()
-		{
-			try
-			{
-				Log.v(LOGTAG,"get categorys data form server begin");
-				HttpGet request = new HttpGet(QuhaoConstant.HTTP_URL + "MerchantController/getTopMerchants?x=6");
+		public void run() {
+			try {
+				Log.v(LOGTAG, "get categorys data form server begin");
+				HttpGet request = new HttpGet(QuhaoConstant.HTTP_URL
+						+ "MerchantController/getTopMerchants?x=6");
 				HttpClient httpClient = new DefaultHttpClient();
 				HttpResponse response = httpClient.execute(request);
-				Log.v(LOGTAG, "get top merchant data form server : " + response.getStatusLine().getStatusCode());
-				if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK)
-				{
+				Log.v(LOGTAG, "get top merchant data form server : "
+						+ response.getStatusLine().getStatusCode());
+				if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
 					String buf = EntityUtils.toString(response.getEntity());
-					Log.v(LOGTAG, "get top merchant data form server buf : " + buf);
+					Log.v(LOGTAG, "get top merchant data form server buf : "
+							+ buf);
 					// 返回HTML页面
 					if (buf.indexOf("<html>") != -1
 							|| buf.indexOf("<HTML>") != -1) {
-						//mGetHandler.sendMessage(mGetHandler
-						//		.obtainMessage(-2));
+						// mGetHandler.sendMessage(mGetHandler
+						// .obtainMessage(-2));
 						throw new Exception("session timeout!");
 					}
-					
-					if(null == topMerchants)
-					{
+
+					if (null == topMerchants) {
 						topMerchants = new ArrayList<TopMerchant>();
 					}
-					
+
 					topMerchants.addAll(ParseJson.getTopMerchants(buf));
-					
-					topMerchantsUpdateHandler.obtainMessage(200,topMerchants).sendToTarget();
-					
+
+					topMerchantsUpdateHandler.obtainMessage(200, topMerchants)
+							.sendToTarget();
 				}
-				
-				
-			} catch (Exception e)
-			{
+
+			} catch (Exception e) {
 				e.printStackTrace();
-			}
-			finally
-			{
+			} finally {
 				progressTopMerchant.closeProgress();
 			}
 		}
 	};
-	
-	private Runnable categoryRunnable = new Runnable()
-	{
-		
+
+	private Runnable categoryRunnable = new Runnable() {
 		@Override
-		public void run()
-		{
-			try
-			{
-				Log.v(LOGTAG,"get categorys data form server begin");
+		public void run() {
+			try {
+				Log.v(LOGTAG, "get categorys data form server begin");
 				String userHome = System.getProperty("user.home");
 				System.out.println(userHome);
-				if(userHome.contains("eacfgjl")){
+				if (userHome.contains("eacfgjl")) {
 					QuhaoConstant.HTTP_URL = "http://146.11.24.199:9081/";
 				}
-				HttpGet request = new HttpGet(QuhaoConstant.HTTP_URL + "MerchantController/allCategories");
+				HttpGet request = new HttpGet(QuhaoConstant.HTTP_URL
+						+ "MerchantController/allCategories");
 				HttpClient httpClient = new DefaultHttpClient();
 				HttpResponse response = httpClient.execute(request);
-				Log.v(LOGTAG, "get categorys data form server : " + response.getStatusLine().getStatusCode());
-				if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK)
-				{
+				Log.v(LOGTAG, "get categorys data form server : "
+						+ response.getStatusLine().getStatusCode());
+				if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
 					String buf = EntityUtils.toString(response.getEntity());
 					Log.v(LOGTAG, "get categorys data form server buf : " + buf);
 					// 返回HTML页面
 					if (buf.indexOf("<html>") != -1
 							|| buf.indexOf("<HTML>") != -1) {
-						//mGetHandler.sendMessage(mGetHandler
-						//		.obtainMessage(-2));
+						// mGetHandler.sendMessage(mGetHandler
+						// .obtainMessage(-2));
 						throw new Exception("session timeout!");
 					}
-					
-					if(null == categorys)
-					{
+
+					if (null == categorys) {
 						categorys = new ArrayList<Category>();
 					}
-					
+
 					categorys.addAll(ParseJson.getCategorys(buf));
-					categorysUpdateHandler.obtainMessage(200,categorys).sendToTarget();
+					categorysUpdateHandler.obtainMessage(200, categorys)
+							.sendToTarget();
 				}
-				
-				
-			} catch (Exception e)
-			{
+
+			} catch (Exception e) {
 				e.printStackTrace();
-			}
-			finally
-			{
+			} finally {
 				progressCategory.closeProgress();
 			}
 		}
 	};
 
 	@Override
-	public void onClick(View v)
-	{
-		
+	public void onClick(View v) {
+
 	}
 
 	@Override
-	public boolean onTouch(View v, MotionEvent event)
-	{
+	public boolean onTouch(View v, MotionEvent event) {
 		return false;
 	}
 }
