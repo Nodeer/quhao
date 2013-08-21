e855f96469f400851dd0b31c5289dfb8ea88c919
Cross_Lee <mag_lee@126.com>
2013-08-21 10:31:29 +0800

    QHClient update

--- modules/android/Quhao/src/com/withiter/quhao/QHClientApplication.java
+++ modules/android/Quhao/src/com/withiter/quhao/QHClientApplication.java
@@ -9,56 +9,41 @@ import com.withiter.quhao.util.db.DBException;
 import com.withiter.quhao.util.tool.DBTools;
 import com.withiter.quhao.util.tool.InfoHelper;
 
-public class QHClientApplication extends Application 
-{
+public class QHClientApplication extends Application {
 
-	/**
-	 * 
-	 */
 	private static final String TAG = "QHClientApplication";
-	
 	public boolean isLogined = false;
-	
 	public AccountInfo accessInfo = null;
-
 	public boolean isAuto = false;
-	
 	private static final String CREATE_ACCOUNT_TABLE = "CREATE TABLE "
 			+ " accountinfo (" + AccountInfoColumn._ID
-			+ " integer primary key autoincrement,"
-			+ AccountInfoColumn.PHONE + " text,"
-			+ AccountInfoColumn.EMAIL + " text,"
+			+ " integer primary key autoincrement," + AccountInfoColumn.PHONE
+			+ " text," + AccountInfoColumn.EMAIL + " text,"
 			+ AccountInfoColumn.PASSWORD + " text,"
 			+ AccountInfoColumn.NICKNAME + " text,"
 			+ AccountInfoColumn.BIRTHDAY + " text,"
-			+ AccountInfoColumn.USERIMAGE + " text,"
-			+ AccountInfoColumn.ENABLE + " text,"
-			+ AccountInfoColumn.MOBILEOS + " text,"
-			+ " isAuto text,"
-			+ AccountInfoColumn.LASTLOGIN + " text)";
+			+ AccountInfoColumn.USERIMAGE + " text," + AccountInfoColumn.ENABLE
+			+ " text," + AccountInfoColumn.MOBILEOS + " text,"
+			+ " isAuto text," + AccountInfoColumn.LASTLOGIN + " text)";
 
 	/**
 	 * 单例
 	 */
 	private static QHClientApplication instance;
 
-	public QHClientApplication() 
-	{
+	public QHClientApplication() {
 		super();
 	}
 
-	public static QHClientApplication getInstance() 
-	{
-		if (instance == null)
-		{
+	public static QHClientApplication getInstance() {
+		if (instance == null) {
 			throw new IllegalStateException("Not yet initialized");
 		}
 		return instance;
 	}
 
 	@Override
-	public void onCreate()
-	{
+	public void onCreate() {
 		Log.d(TAG, "onCreate");
 		isLogined = false;
 		instance = this;
@@ -67,39 +52,28 @@ public class QHClientApplication extends Application
 		super.onCreate();
 	}
 
-	
-
-	private void initDBConfig()
-	{
+	private void initDBConfig() {
 		Log.d(TAG, "init database config");
 		accessInfo = InfoHelper.getAccountInfo(this);
-		
 		DBTools.init(instance);
-		
 		String sql = CREATE_ACCOUNT_TABLE;
-		
 		createTable("accountinfo", sql);
 		sql = null;
-		
 	}
-	
-	private void createTable(String tableName, String sql)
-	{
-		try
-		{
+
+	private void createTable(String tableName, String sql) {
+		try {
 			DBTools.getInstance().creatTable(tableName, false, sql);
-		} catch (DBException e)
-		{
+		} catch (DBException e) {
 			Log.d(TAG, "The table " + tableName
 					+ " already exist, create failure");
 		}
-		
+
 	}
 
-	private void initConfig()
-	{
+	private void initConfig() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
