6a113535e173c4472d46c402636aefd5fe435676
Cross_Lee <mag_lee@126.com>
2013-08-21 10:17:47 +0800

    format adapter package's codes

--- modules/android/Quhao/src/com/withiter/quhao/adapter/MerchantAdapter.java
+++ modules/android/Quhao/src/com/withiter/quhao/adapter/MerchantAdapter.java
@@ -21,129 +21,120 @@ import com.withiter.quhao.util.AsyncImageLoader;
 import com.withiter.quhao.util.AsyncImageLoader.ImageCallback;
 import com.withiter.quhao.vo.Merchant;
 
-public class MerchantAdapter extends BaseAdapter
-{
-	
+public class MerchantAdapter extends BaseAdapter {
+
 	private ListView listView;
-	
 	public List<Merchant> merchants;
-	
 	private Activity activity;
-	
 	private AsyncImageLoader asyncImageLoader;
-	
+
 	public MerchantAdapter(Activity activity, ListView listView,
-			List<Merchant> merchants)
-	{
+			List<Merchant> merchants) {
 		super();
 		this.activity = activity;
 		this.listView = listView;
 		this.merchants = merchants;
 		asyncImageLoader = new AsyncImageLoader();
-		
+
 	}
-	
-	
 
 	@Override
-	public int getCount()
-	{
+	public int getCount() {
 		return merchants.size();
 	}
 
 	@Override
-	public Object getItem(int position)
-	{
+	public Object getItem(int position) {
 		return merchants.get(position);
 	}
 
 	@Override
-	public long getItemId(int position)
-	{
+	public long getItemId(int position) {
 		return position;
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent)
-	{
-		
+	public View getView(int position, View convertView, ViewGroup parent) {
 		Drawable cachedImage = null;
 		Merchant merchant = (Merchant) getItem(position);
-		synchronized (merchant)
-		{
+		synchronized (merchant) {
 			ViewHolder holder = null;
-			if(convertView == null)
-			{
+			if (convertView == null) {
 				holder = new ViewHolder();
-				LayoutInflater inflator = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-				convertView = inflator.inflate(R.layout.merchant_list_item, null);
+				LayoutInflater inflator = (LayoutInflater) parent.getContext()
+						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				convertView = inflator.inflate(R.layout.merchant_list_item,
+						null);
 				holder.img = (ImageView) convertView.findViewById(R.id.img);
-				holder.btn = (Button) convertView.findViewById(R.id.btnMerchantDetail);
-				holder.content = (TextView) convertView.findViewById(R.id.merchantName);
-				holder.btnEnter = (TextView) convertView.findViewById(R.id.merchantAddress);
-				
-				/* 重新设置图片的宽高 
-				holder.img.setScaleType(ImageView.ScaleType.CENTER_INSIDE);*/
-				/* 重新设置Layout 的宽高 
-				holder.img.setLayoutParams(new LinearLayout.LayoutParams(
-						LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));*/
-				
+				holder.btn = (Button) convertView
+						.findViewById(R.id.btnMerchantDetail);
+				holder.content = (TextView) convertView
+						.findViewById(R.id.merchantName);
+				holder.btnEnter = (TextView) convertView
+						.findViewById(R.id.merchantAddress);
+
+				/*
+				 * 重新设置图片的宽高
+				 * holder.img.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+				 */
+				/*
+				 * 重新设置Layout 的宽高 holder.img.setLayoutParams(new
+				 * LinearLayout.LayoutParams( LayoutParams.WRAP_CONTENT,
+				 * LayoutParams.WRAP_CONTENT));
+				 */
+
 			}
 			if (holder == null) {
 				holder = (ViewHolder) convertView.getTag();
 			}
-			
+
 			String imageUrl = merchant.imgUrl;
-			
+
 			holder.img.setTag(imageUrl);
-			if(null != imageUrl && !"".equals(imageUrl))
-			{
-				cachedImage = asyncImageLoader.loadDrawable(imageUrl, new ImageCallback()
-				{
-					
-					@Override
-					public void imageLoaded(Drawable imageDrawable, String imageUrl)
-					{
-						ImageView imageViewByTag = (ImageView) listView.findViewWithTag(imageUrl);
-						if(null != imageViewByTag && null != imageDrawable)
-						{
-							imageViewByTag.setImageDrawable(imageDrawable);
-							imageViewByTag.invalidate();
-							imageDrawable.setCallback(null);
-							imageDrawable = null;
-						}
-						
-					}
-				});
-				
+			if (null != imageUrl && !"".equals(imageUrl)) {
+				cachedImage = asyncImageLoader.loadDrawable(imageUrl,
+						new ImageCallback() {
+
+							@Override
+							public void imageLoaded(Drawable imageDrawable,
+									String imageUrl) {
+								ImageView imageViewByTag = (ImageView) listView
+										.findViewWithTag(imageUrl);
+								if (null != imageViewByTag
+										&& null != imageDrawable) {
+									imageViewByTag
+											.setImageDrawable(imageDrawable);
+									imageViewByTag.invalidate();
+									imageDrawable.setCallback(null);
+									imageDrawable = null;
+								}
+
+							}
+						});
+
 			}
-			//设置图片给imageView 对象
-			if(null != cachedImage)
-			{
+			// 设置图片给imageView 对象
+			if (null != cachedImage) {
 				holder.img.setImageDrawable(cachedImage);
 				holder.img.invalidate();
 				cachedImage.setCallback(null);
 				cachedImage = null;
-			}
-			else
-			{
+			} else {
 				holder.img.setImageResource(R.drawable.title_img);
 			}
-			
+
 			holder.content.setTag("content_" + position);
-			
+
 			holder.btnEnter.setTag("btnEnter_" + position);
 			holder.content.setText(merchant.name);
 			holder.btnEnter.setText(merchant.address);
 			convertView.setTag(holder);
 			return convertView;
 		}
-		
+
 	}
 
-	
-	class ViewHolder
-	{
+	class ViewHolder {
 		ImageView img;
 		TextView content;
 		TextView btnEnter;
