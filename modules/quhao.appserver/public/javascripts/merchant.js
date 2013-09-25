d2eb701d6139934fcb89f496706f26afc8896e15
Cross_Lee <mag_lee@126.com>
2013-09-25 14:33:14 +0800

    finish reset button function

---
+++ modules/quhao.appserver/public/javascripts/merchant.js
@@ -0,0 +1,15 @@
+Merchant = {};
+
+/**
+ * reset the merchant infomation form
+ */
+Merchant.reset = function(){
+	
+	$("#description").val("");
+	$("#merchantImage").val("");
+	$("#address").val("");
+	$("#tel").val("");
+	$("#cateType").val("");
+	$("#openTime").val("");
+	$("#closeTime").val("");
+}
