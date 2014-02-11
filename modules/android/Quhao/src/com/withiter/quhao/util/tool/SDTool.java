43608dabf046c2c1d2cb6f22c0ced23213b0a815
Cross_Lee <mag_lee@126.com>
2014-02-11 13:35:50 +0800

    Checks if external storage is available for read and write

--- modules/android/Quhao/src/com/withiter/quhao/util/tool/SDTool.java
+++ modules/android/Quhao/src/com/withiter/quhao/util/tool/SDTool.java
@@ -50,4 +50,23 @@ public class SDTool {
 		// 返回SD卡大小
 		return allBlocks * blockSize; // 单位Byte
 	}
+	
+	/* Checks if external storage is available for read and write */
+	public boolean isExternalStorageWritable() {
+	    String state = Environment.getExternalStorageState();
+	    if (Environment.MEDIA_MOUNTED.equals(state)) {
+	        return true;
+	    }
+	    return false;
+	}
+
+	/* Checks if external storage is available to at least read */
+	public boolean isExternalStorageReadable() {
+	    String state = Environment.getExternalStorageState();
+	    if (Environment.MEDIA_MOUNTED.equals(state) ||
+	        Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+	        return true;
+	    }
+	    return false;
+	}
 }
