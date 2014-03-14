1d939e79ae4209cc5ad275cc072b3e9cb0b19422
Cross Lee <mag_lee@126.com>
2014-03-14 11:31:34 +0800

    enhancement for paidui status
    http://localhost:9081/pStatus?mid=52e3c726036431505d9a9e20

--- modules/quhao.appserver/app/controllers/ReservationController.java
+++ modules/quhao.appserver/app/controllers/ReservationController.java
@@ -1,8 +1,22 @@
 package controllers;
 
+import vo.HaomaVO;
+
+import com.withiter.models.merchant.Haoma;
+
 public class ReservationController extends BaseController{
 
 	public static void refreshPaidui(String aid, String mid, int seatNumber){
 		
+	}
+	
+	public static void paiduiStatusForApp(){
+		String mid = params.get("mid");
+		Haoma haoma = Haoma.findByMerchantId(mid);
+
+		haoma.updateSelf();
+
+		HaomaVO haomaVO = HaomaVO.build(haoma);
+		renderJSON(haomaVO);
 	}
 }
