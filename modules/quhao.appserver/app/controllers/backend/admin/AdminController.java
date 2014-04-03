7db804965c79d9181505cb5771c913a2beff5d45
Cross Lee <mag_lee@126.com>
2014-04-03 15:30:41 +0800

    add admin page

---
+++ modules/quhao.appserver/app/controllers/backend/admin/AdminController.java
@@ -0,0 +1,10 @@
+package controllers.backend.admin;
+
+import controllers.BaseController;
+
+public class AdminController extends BaseController {
+
+	public static void index(){
+		renderJapid();
+	}
+}
