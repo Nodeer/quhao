c1f2df70963bfa663731409f800aabfa947a3ca1
Cross Lee <mag_lee@126.com>
2014-04-22 15:52:56 +0800

    code clean

--- modules/quhao.appserver/app/com/withiter/jobs/OneTimePatchJob.java
+++ modules/quhao.appserver/app/com/withiter/jobs/OneTimePatchJob.java
@@ -10,58 +10,6 @@ public class OneTimePatchJob extends Job {
 
 	@Override
 	public void doJob() throws Exception {
-//		SchoolIdentityService.getInstance().createWriter();
-//		try {
-//			HighSchoolImporter.importHighSchools();
-//		} catch (Exception e) {
-//			Logger.error(e, "Import high school data failed");
-//		}
-//		try {
-//			USCityImporter.importUSCitys();
-//		} catch (Exception e) {
-//			Logger.error(e, "Import US City data failed");
-//		}
-//		try {
-//			TestScoresImporter.importTestScores();
-//		} catch (Exception e) {
-//			Logger.error(e, "Import test scores data failed");
-//		}
-//
-//		try {
-//			SchoolDeadlineImporter.importSchoolDeadlines();
-//		} catch (Exception e) {
-//			Logger.error(e, "Import School Deadline data failed");
-//		}
-//		try {
-//			SchoolRelatedImporter.importSchoolRelateds();
-//		} catch (Exception e) {
-//			Logger.error(e, "Import School Related data failed");
-//		}
-//
-//		try {
-//			SchoolLogosImporter.importSchoolRelateds();
-//		} catch (Exception e) {
-//			Logger.error(e, "Import School Related data failed");
-//		}
-//
-//		try {
-//			SchoolPropertyImporter.importSchoolProperty();
-//		} catch (Exception e) {
-//			Logger.error(e, "Import School Property data failed");
-//		}
-//
-//		try {
-//			DefaultCollectionsImporter.importDefaultCollections();
-//		} catch (Exception e) {
-//			Logger.error(e, "Import Default Collections Error");
-//		}
-//
-//		try {
-//			MorphiaPlugin.ds().getMongo().getDB("applyful-dev-db").getCollection("School").ensureIndex(new BasicDBObject("latLng", "2d"));
-//		} catch (Exception e) {
-//			Logger.error(e, "Init MongoDB Error");
-//		}
-//
 		OnetimePatch.registerAllOnetimePatch();
 		OnetimePatch.executeAll();
 	}
