31bc76fad43a7ea5687d2ceae927136922dc8cab
Cross Lee <mag_lee@126.com>
2014-05-29 18:14:59 +0800

    add clean chat room job

---
+++ modules/chat.appserver/app/jobs/CleanChatRoomJob.java
@@ -0,0 +1,21 @@
+package jobs;
+
+
+import models.ChatRoomFactory;
+import play.Logger;
+import play.jobs.Job;
+import play.jobs.On;
+
+//每天2:00AM开始job
+@On("0 0 2 * * ?")
+public class CleanChatRoomJob extends Job {
+
+	@Override
+	public void doJob() throws Exception {
+		Logger.info("start to clean chat rooms map");
+		Logger.info("current chat rooms size is : " + ChatRoomFactory.rooms().size());
+		long start = System.currentTimeMillis();
+		ChatRoomFactory.rooms().clear();
+		Logger.info("finished, elapsed time " + (System.currentTimeMillis() - start) + "ms.");
+	}
+}
