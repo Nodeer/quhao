dff75e08d715bbb0abbb22b346c23b589b4b1e42
Cross Lee <mag_lee@126.com>
2014-06-04 16:58:40 +0800

    chat debug

--- modules/chat.appserver/app/controllers/WebSocket.java
+++ modules/chat.appserver/app/controllers/WebSocket.java
@@ -48,7 +48,8 @@ public class WebSocket extends Controller {
 			}
 			
 			Logger.debug("This server rooms counts are : %d. current room id is %s.", rooms.size(), room.mid);
-			
+			Logger.debug("room.socketNumber %d", room.socketNumber);
+			Logger.debug("Integer.parseInt(roomUserLimit) %d", Integer.parseInt(roomUserLimit));
 			if(room.socketNumber >= Integer.parseInt(roomUserLimit)){
 				return;
 			}
