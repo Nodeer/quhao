ba6b98bed537377d23858221eefe6e81e4cce3e2
Cross Lee <mag_lee@126.com>
2014-06-30 15:46:43 +0800

    add before verification

--- modules/quhao.appserver/app/controllers/ShareController.java
+++ modules/quhao.appserver/app/controllers/ShareController.java
@@ -7,6 +7,7 @@ import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.bson.types.ObjectId;
@@ -15,12 +16,14 @@ import org.slf4j.LoggerFactory;
 
 import play.Play;
 import play.modules.morphia.Model.MorphiaQuery;
+import play.mvc.Before;
 import vo.ShareVO;
 
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.CommandResult;
 import com.mongodb.gridfs.GridFSInputFile;
+import com.withiter.common.Constants;
 import com.withiter.models.social.Share;
 
 public class ShareController extends BaseController {
@@ -28,6 +31,31 @@ public class ShareController extends BaseController {
 	public static final int  NUMBER_PER_PAGE = 10;
 	private static Logger logger = LoggerFactory.getLogger(ShareController.class);
 
+	/**
+	 * Interception any caller on this controller, will first invoke this method
+	 */
+	@Before
+	static void checkAuthentification() {
+		if(session.contains(Constants.SESSION_USERNAME)){
+			return;
+		}
+		
+		Map headers = request.headers;
+		Iterator it = headers.keySet().iterator();
+		while(it.hasNext()){
+			String key = (String) it.next();
+			logger.debug(key+", " +headers.get(key));
+		}
+		
+		if(headers.containsKey("user-agent")){
+			if(!(request.headers.get("user-agent").values.contains("QuhaoAndroid") || request.headers.get("user-agent").values.contains("QuhaoIOS"))){
+				renderJSON("请使用Android/iOS APP访问。");
+			}
+		} else {
+			renderJSON("请使用Android/iOS APP访问。");
+		}
+	}
+	
 	/**
 	 * 获取周边分享
 	 * @param page
